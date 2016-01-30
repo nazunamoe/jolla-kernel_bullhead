5597875e16975ef234d2e41841e93298ecf24a5c
nazunamoe <jpg3927@gmail.com>
2016-01-30 17:38:33 +0900

    build_kernel.sh:  make zip package for flashing

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Welcome to All-Automated Kernel Launching System by nazunamoe"
+
 #First of all, We need to make directory clean
 make mrproper
 
@@ -9,7 +11,18 @@ export ARCH=arm64
 #I use UBERTC 6.0 from my local directory, you need to change this for use
 export CROSS_COMPILE=~/ubertc/aarch64-linux-android-6.0-kernel/bin/aarch64-linux-android-
 
-#make defconfig and here we go!
+#Make defconfig and here we go!
 make bullhead_defconfig
-
+echo "Let's Make zImage!"
 make -j64
+
+#Check zImage.gz-dtb
+file="/home/nazunamoe/jolla/arch/arm64/boot/Image.gz-dtb"
+if [ -e $file ];then
+echo "Build succeed let's make zip package to flash"
+cp $file /home/nazunamoe/zip/jolla-kernel/bullhead/Image
+zip -r /home/nazunamoe/jolla-kernel_AIO_bullhead-unoffical-$(date '+%g%m%d').zip /home/nazunamoe/zip/jolla-kernel/*
+echo "Make succeed!"
+else
+echo "Build Failed"
+fi
