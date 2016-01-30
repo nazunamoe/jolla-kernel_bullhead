907690b6d334a015a2982f6d18c2727ea556aa11
nazunamoe <jpg3927@gmail.com>
2016-01-30 16:57:15 +0900

    build_kernel.sh: add descripction and make mrproper to clean build

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+#First of all, We need to make directory clean
+make mrproper
+
+#Set Build Environment for Google Nexus 5X
 export ARCH=arm64
+
+#I use UBERTC 6.0 from my local directory, you need to change this for use
 export CROSS_COMPILE=~/ubertc/aarch64-linux-android-6.0-kernel/bin/aarch64-linux-android-
 
+#make defconfig and here we go!
 make bullhead_defconfig
 
 make -j64
